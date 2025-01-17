4ad4931294d03b6d65d4cb2e2924754e888a65bb
sandip25-devil <nayaksandip453@gmail.com>
2025-01-17 09:35:45 +0530

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Oddeven{
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int number = scan.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + "is even");
+
+        }else{
+            System.out.println(number + "is odd");
+        }
+    }
+}
